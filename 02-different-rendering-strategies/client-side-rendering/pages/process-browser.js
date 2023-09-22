40186dd1a6437d23266f1cdf5fbcb392255e9756
cheolsker <cheolsker@MacBook-Pro-2.local>
2023-09-22 21:08:36 +0900

    ch2 : process 대신 typeof window를 사용할 수도 있다.

--- 02-different-rendering-strategies/client-side-rendering/pages/process-browser.js
+++ 02-different-rendering-strategies/client-side-rendering/pages/process-browser.js
@@ -1,10 +1,13 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 function ProcessBrowserPage() {
+  // const side = process.browser ? "client" : "server";
+  const side = typeof window === "undefined" ? "server" : "client";
+
   return (
     <>
       <main className={styles.main}>
-        <p> I'm running on the {process.browser ? 'client' : 'server'} </p>
+        <p> I'm running on the {side} </p>
       </main>
     </>
   );
